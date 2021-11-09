3821c8058229dedd2d9b9def37e9140fec5204c4
klreeves30 <klreeves30@gmail.com>
2021-11-09 09:48:23 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var celsius =  TempConverter.FahrenheitToCelsius(75);
+            Console.WriteLine($"fahrenheit to celcius is: {celsius}\n");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(18);
+            Console.WriteLine($"celsius to fahrenheit is: {fahrenheit}\n");
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeywordExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result =  (fahrenheit - 32) / 1.8;
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            var result = (celsius * 9) / 5 + 32;
+            return result;
+        }
+
+    }
+}
+   
